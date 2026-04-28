3016bbeb7379d9001ff54b363cc1be850757f4aa
abdurakhimov01 <sololeveling1958@gmail.com>
2026-04-28 11:14:43 +0500

    salom03"

--- file03.py
+++ file03.py
@@ -4,8 +4,13 @@ def main(filename):
     p=f.read()
     l=[]
     d=string.digits
-    for i in p:
-        if i in d:
-            l.append(i)
+    i=0
+    while i <len(p):
+        j=0
+        while j<len(d):
+            if d[j]==p[i]:
+                l.append(p[i])
+            j+=1
+        i+=1
     return l
 print(main("data/data03.txt"))
