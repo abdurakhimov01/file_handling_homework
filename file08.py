1d9d34ac2d3bb38663aa2238faf6264e1aa876e3
abdurakhimov01 <sololeveling1958@gmail.com>
2026-04-28 12:16:52 +0500

    salom08

--- file08.py
+++ file08.py
@@ -1,3 +1,4 @@
+import string
 def main(data:str):
     """
     The data is from the file. Find the largest of the numeric characters.
@@ -6,5 +7,18 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    f = open(data, 'r')
+    p=f.read()
+    l=[]
+    d=string.digits
+    i=0
+    while i <len(p):
+        j=0
+        while j<len(d):
+            if d[j]==p[i]:
+                l.append(p[i])
+            j+=1
+        i+=1
+    return max(l)
+print(main("data/data08.txt"))
 # Read data from file
