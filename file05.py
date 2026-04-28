2c34af10ca1d8cae9343f20485a95044de9adffe
abdurakhimov01 <sololeveling1958@gmail.com>
2026-04-28 10:09:55 +0500

    salom05
    git push
    exit

--- file05.py
+++ file05.py
@@ -1,10 +1,16 @@
-def main(data:str):
-    """
-    The data is from the file. Find the number of digital and str(non-digital) data and return as list type.
-    Args:
-        data: str
-    Returns:
-        list: return answer
-    """
-    
-# Read data from file
+import string
+def main(filename):
+    o=open(filename,"r")
+    f=o.read()
+    d=string.digits
+    l=string.ascii_letters
+    i=0
+    k=0
+    g=0
+    for i in f:
+        if i in string.digits:
+            k+=1
+        elif i in string.ascii_letters:
+            g+=1
+    return k,g
+print(main("data/data05.txt"))
