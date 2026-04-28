41102e0f4009f4ee8ecc459d2d55e25a909ca488
abdurakhimov01 <sololeveling1958@gmail.com>
2026-04-28 12:19:08 +0500

    salom09

--- file09.py
+++ file09.py
@@ -1,3 +1,4 @@
+import string
 def main(data:str):
     """
     The data is from the file. Find the smallest of the numeric characters.
@@ -6,5 +7,18 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    f = open(data, 'r')
+    p=f.read()
+    l=[]
+    d=string.digits
+    i=0
+    while i <len(p):
+        j=0
+        while j<len(d):
+            if d[j]==p[i]:
+                l.append(p[i])
+            j+=1
+        i+=1
+    return min(l)
+print(main("data/data09.txt"))
 # Read data from file
